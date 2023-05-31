3608fd317c37135a9550e925f52f96ff03cc2a37
DWK <DWK@DESKTOP-HGJB1HB>
2023-05-31 18:35:20 +0700

    Add project files.

---
+++ testvariable/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace testvariable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btndisplay
+            // 
+            this.btndisplay.Location = new System.Drawing.Point(76, 78);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(84, 46);
+            this.btndisplay.TabIndex = 0;
+            this.btndisplay.Text = "Display";
+            this.btndisplay.UseVisualStyleBackColor = true;
+            this.btndisplay.Click += new System.EventHandler(this.btndisplay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 340);
+            this.Controls.Add(this.btndisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndisplay;
+    }
+}
+
---
+++ testvariable/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testvariable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndisplay_Click(object sender, EventArgs e)
+        {
+            int id;
+            id = 123;
+            id = 1234;
+
+            MessageBox.Show("Your id :" + id);
+
+            string name;
+            name = "sok";
+            MessageBox.Show("your name :" + name);
+
+            double salary;
+            salary = 1000.50;
+            MessageBox.Show("your salary :" + salary);
+
+            char gender;
+            gender = 'F';
+            MessageBox.Show("your gender is :" + gender);
+
+
+            bool kh; // Boolean
+            kh = true;
+            if (kh==true) {
+                MessageBox.Show("You are khmer ");
+            }
+
+            int x = 5;
+            ++x; //x = x + 1;
+            MessageBox.Show("value x :" + x);
+
+            int x1 = 5;
+            MessageBox.Show("value xx :" + x1++);
+
+            float a, b,s; // both a and b have the same datatype
+            a = 10; b = 5; s = a + b; MessageBox.Show(" a + b =" + s);
+
+        }
+    }
+}
